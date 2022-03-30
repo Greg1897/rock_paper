2627b8697435d82a78838ebc804a5fbb8c500697
Greg1897 <gregory1897tifosi@gmail.com>
2022-03-30 19:25:56 +0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World!!!")
+function computerPlay() {
+    let choice = Math.random() * 3;
+    if (choice <= 1) {
+    return (choice = "rock");
+    } else if (choice <= 2) {
+    return (choice = "paper");
+    } else {
+    return (choice = "scissors");
+    }
+}
+let computerSelection
+computerSelection = computerPlay()
+console.log(computerSelection)
